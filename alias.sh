25934b43c9b0c9f0b3b36ada234bb4fbde9155a7
jakobpederson <jakob.pederson+github@gmail.com>
2017-09-12 15:53:08 -0500

    initial commit

---
+++ alias.sh
@@ -0,0 +1,3 @@
+alias ec2="AWS_DEFAULT_PROFILE=eb-cli /Users/imtapps/.virtualenvs/aws-fuzzy/bin/aws-fuzzy --key-path ~/.ssh/staging-key-bastion.pem --tunnel --tunnel-key-path 'staging-key-beanstalk.pem'"
+alias ec2-prod="AWS_DEFAULT_PROFILE=eb-cli /Users/imtapps/.virtualenvs/aws-fuzzy/bin/aws-fuzzy --key-path ~/.ssh/production-key-bastion.pem --tunnel --tunnel-key-path 'production-key-beanstalk.pem'"
+alias kill-cam="sudo killall VDCAssistant; sudo killall AppleCameraAssistant"
